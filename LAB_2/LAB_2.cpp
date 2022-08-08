802269aeb83e1c3eadbf234d58db7bee4d1fe57a
Nozpher <66372864+Nozpher@users.noreply.github.com>
2022-08-08 16:46:34 +0500

    Добавьте файлы проекта.

---
+++ LAB_2/LAB_2.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+#include <clocale>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double eps;
+    int n = 1;
+    double N, a , Sum;
+    Sum = 0.0;
+    a = 0.0;
+    N = 1.0;
+
+    cout << "Введите вещественное eps>0 " << endl;
+    cin >> eps;
+    while (a < eps) {
+        N = N * n;
+        a = pow(10, n)/(N);
+        Sum += a;
+        n = n + 1;
+    }
+    cout << "Сумма ряда,вычисленная с заданной точностью  =  " << Sum << endl;
+    system("pause");
+    return 0;
+}
+
